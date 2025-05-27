7bdffef7c31d4fc068a76926b5bc88be9589cc71
Abhimanyu Govindaraju <y.g.abhimanyu@gmail.com>
2025-05-27 20:58:36 +0530

    Adding simple main
    - Initialize logger
    - Initialize session manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
-#include <iostream>
+#include "common.hpp"
+#include "lv2PluginMgr.hpp"
+#include "sessionMgr.hpp"
 
-int main(void) { return 0; }
+int main(void) {
+
+  Logger::init("calicofx");
+  SessionMgr session;
+
+  session.createNode("http://guitarix.sourceforge.net/plugins/gxautowah#wah");
+
+  while (1);
+
+
+
+  return 0;
+}
